7478677c6617897f2d95126efc968f3b42610f91
HungT <hungt@thefirstone.jp>
2026-04-23 14:44:39 +0700

    Cap nhat README va thiet ke 3 bang CSDL

---
+++ database/script.sql
@@ -0,0 +1,27 @@
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE positions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE employees (
+    emp_id VARCHAR(50) PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    base_salary BIGINT NOT NULL,
+    actual_salary BIGINT NOT NULL,
+    birthday DATE NOT NULL,
+    department_id INT,
+    position_id INTEGER,
+    FOREIGN KEY (department_id) REFERENCES departments(id),
+    FOREIGN KEY (position_id) REFERENCES positions(id)
+);
+
+INSERT INTO departments (name)
+VALUES ('Phòng IT'), ('Phòng Kế toán'), ('Phòng Nhân sự');
+INSERT INTO positions (name)
+VALUES ('Giám đốc'), ('Trưởng phòng'), ('Nhân viên');
