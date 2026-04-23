049a60b45d64328ef1c62cc14473969076002433
HungT <hungt@thefirstone.jp>
2026-04-23 15:00:41 +0700

    Config: Tao class Database ket noi PDO

---
+++ config/Database.php
@@ -0,0 +1,19 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "staff_manager";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db_name;charset=utf8mb4", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+        return $this->conn;
+    }
+}
